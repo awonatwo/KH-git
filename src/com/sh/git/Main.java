0f805b034ae6b841aabb7592430fe027d37886bf
awonatwo <greenfone42@gmail.com>
2022-12-19 11:27:46 +0900

    1

--- src/com/sh/git/Main.java
+++ src/com/sh/git/Main.java
@@ -7,7 +7,10 @@ package com.sh.git;
  * - reset : 지정한 커밋으로 돌아간다.
  * 		- hard : 지정 커밋 이후 변경사항 버림
  * 		- mixed : 지정 커밋 이후 변경사항을 unstaging area에 보관. 재추가 후 커밋 가능.
- * 		- soft : 지정 커밋 이후 변경사항을 staging area에 보관. 즉시 커밋 가능
+ * 		- soft : 지정 커밋 이후 변경사항을 staging area에 보관. 즉시 커밋 가능.
+ * 
+ * - branch
+ * 		master >> feat1추가. branch분화. >> 작업이 잘 안되면 잘라버리고, 잘 되면 마스터에서 feat1을 합쳐. 코드를 가져옴 :merge 버그고칠때
  * 
  */
 public class Main {
